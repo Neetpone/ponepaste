ed782ced56528f1a2ee4a141cdcad8385b3c1f4c
aftercase <74765798+aftercase@users.noreply.github.com>
2021-08-16 18:58:47 +0100

    added load animation
    Added load animation for archive page when it's searching.

--- theme/bulma/archive.php
+++ theme/bulma/archive.php
@@ -20,6 +20,7 @@
             rowReorder: { selector: 'td:nth-child(2)'},
             responsive: true,
             processing: true,
+            language: {processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span> '},
             autoWidth: false,
             ajax: "api/ajax_pastes.php",
             initComplete: function () {
