4103bc48c08475c69c37dd881497df0c03080cbd
Floorb <132411956+Neetpone@users.noreply.github.com>
2021-08-20 16:17:13 -0400

    Fix small reflected XSS

--- includes/common.php
+++ includes/common.php
@@ -154,3 +154,7 @@ $current_user = User::current($conn);
 if ($current_user) {
     $noguests = "off";
 }
+
+/* Security headers */
+header('X-Frame-Options: SAMEORIGIN');
+header('X-Content-Type-Options: nosniff');
