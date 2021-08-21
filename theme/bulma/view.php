259e4132ed756c93284c877504847e286674ab69
aftercase <74765798+aftercase@users.noreply.github.com>
2021-08-22 00:07:43 +0100

    Code clean up.
    Removed unneeded code.

--- theme/bulma/view.php
+++ theme/bulma/view.php
@@ -1,4 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <link rel="stylesheet" href="theme/bulma/css/bulma-tagsinput.min.css"/>
 <script src="theme/bulma/js/bulma-tagsinput.min.js"></script>
 <script>
